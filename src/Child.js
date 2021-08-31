8a948a5cb9dd6b9c823e03e542333f1f9dfd0376
Veronica <veronicasmigielski@gmail.com>
2021-08-31 22:20:59 +0200

    Sent props from child to parent through state/function/onClick

---
+++ src/Child.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Child.css';
+
+function Child(props) {
+    return (
+        <div className="child">
+            <h1>Child</h1>
+            <button onClick={() => props.changeWord('Veronica')}>Click to change title</button>
+        </div>
+    )
+}
+
+export default Child;
